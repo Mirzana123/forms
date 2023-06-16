538cc8a37856a1570ecce2f665b607df6c787be9
ICT ACADEMY <thanvirmuhd@gmail.com>
2023-06-16 17:07:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { useState,useEffect } from 'react';
+import Loginform from './Loginform';
+
+
+
+export default function App() {
+  const [todos, setTodo] = useState([]);
+  
+
+  useEffect(() => {
+    getData();
+
+  },[]);
+  
+  
+  const [check,setCheck] = useState(true);
+  
+  
+  const getData =async () => {
+    try {
+      const incomingData = await fetch (
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+
+      const formattedData = await incomingData.json();
+      setTodo(formattedData);
+      console.log(formattedData);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <Loginform></Loginform>
+      <Container className = "my-5">
+     <h1 className="my-5 text-center display-2">Todo List</h1>
+      <Table striped bordered hover>
+      <thead>
+      </thead>
+      <tbody>
+        
+        {todos.map((todo,i) => (
+          <tr key={todo.id}>
+            <td>{i+1}</td>
+            <td style ={{color:todo.completed?'green':'red'}} >{todo.title}</td>
+          </tr>
+        ))}
+        </tbody>
+    </Table>
+ </Container>
+    </div>
+  )
+}
+
+       
